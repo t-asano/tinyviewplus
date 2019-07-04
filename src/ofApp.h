2786e9438ba0e24eec1384fe904614d79e2a3f56
t-asano <14119184+t-asano@users.noreply.github.com>
2019-07-05 08:16:02 +0900

    v0.9.26

--- src/ofApp.h
+++ src/ofApp.h
@@ -13,7 +13,7 @@
 /* ---------- definitions ---------- */
 
 // system
-#define APP_VER         "v0.9.25"
+#define APP_VER         "v0.9.26"
 #define DEBUG_ENABLED   false
 #define HELP_LINES      33  // must be <= OVLTXT_LINES
 #define SCENE_INIT      0
