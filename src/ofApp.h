db111f38370bdb57542a086205173f333d21e621
t-asano <14119184+t-asano@users.noreply.github.com>
2019-08-19 12:37:04 +0900

    v0.9.27

--- src/ofApp.h
+++ src/ofApp.h
@@ -13,7 +13,7 @@
 /* ---------- definitions ---------- */
 
 // system
-#define APP_VER         "v0.9.26"
+#define APP_VER         "v0.9.27"
 #define DEBUG_ENABLED   false
 #define HELP_LINES      33  // must be <= OVLTXT_LINES
 #define SCENE_INIT      0
