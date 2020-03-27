afe3f7586a6a9ec7ea20bb725028252b7784af92
t-asano <14119184+t-asano@users.noreply.github.com>
2020-03-28 04:46:26 +0900

    v0.9.31

--- src/ofApp.h
+++ src/ofApp.h
@@ -13,7 +13,7 @@
 /* ---------- definitions ---------- */
 
 // system
-#define APP_VER         "v0.9.30"
+#define APP_VER         "v0.9.31"
 #define DEBUG_ENABLED   false
 #define HELP_LINES      35  // must be <= OVLTXT_LINES
 #define SCENE_INIT      0
