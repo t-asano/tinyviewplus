c5b6ac222b2ecd95462c1e5d71fff047b3e4f2ff
t-asano <14119184+t-asano@users.noreply.github.com>
2025-01-13 11:31:59 +0900

    v0.9.33 beta2

--- src/ofApp.h
+++ src/ofApp.h
@@ -13,7 +13,7 @@
 /* ---------- definitions ---------- */
 
 // system
-#define APP_VER         "v0.9.33 beta1"
+#define APP_VER         "v0.9.33 beta2"
 #define DEBUG_ENABLED   false
 #define HELP_LINES      35  // must be <= OVLTXT_LINES
 #define SCENE_INIT      0
